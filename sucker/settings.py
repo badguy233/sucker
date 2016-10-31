a80514dddceca5d0af6c843f86db73d8516f9cb6
461825789 <461825789@qq.com>
2016-10-31 15:38:50 +0800

    modify

--- sucker/settings.py
+++ sucker/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'main',
     'tinymce',
+    'ucenter',
 ]
 
 MIDDLEWARE_CLASSES = [
--- sucker/urls.py
+++ sucker/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^',include('main.urls', namespace='sucker')),
     url(r'^tinymce/', include('tinymce.urls')),
+    url(r'^ucenter/',include('ucenter.urls'))
 ]
